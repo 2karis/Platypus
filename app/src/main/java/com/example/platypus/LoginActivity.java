317759deadd82534212be31f8333cd4ae41ec8c3
2karis <gitauk2@gmail.com>
2019-03-30 16:48:23 -0400

    made changes

--- app/src/main/java/com/example/platypus/LoginActivity.java
+++ app/src/main/java/com/example/platypus/LoginActivity.java
@@ -2,18 +2,20 @@ package com.example.platypus;
 
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText etUsername, etPassword;
-    String username, password;
-    String url = "https://misdirect1.000webhostapp.com/login.php";
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +23,30 @@ public class LoginActivity extends AppCompatActivity {
 
         etUsername = (EditText) findViewById(R.id.username);
         etPassword = (EditText) findViewById(R.id.password);
-        final TextView tvRegisterLink = (TextView) findViewById(R.id.create);
 
-        tvRegisterLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        loginBtn = (Button) findViewById(R.id.login);
+
+        final TextView RegisterLink = (TextView) findViewById(R.id.create);
+
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Log.d(String.valueOf(this.getClass()),"//////////////////////////////////////");
+                Log.d(String.valueOf(this.getClass()),"//////////////////////////////////////");
+                Log.d(String.valueOf(this.getClass()),"//////////////////////////////////////");
+                Log.d(String.valueOf(this.getClass()),"//////////////////////////////////////");
+                Log.d(String.valueOf(this.getClass()),"//////////////////////////////////////");
+                Log.d(String.valueOf(this.getClass()),"//////////////////////////////////////");
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+                 Toast.makeText(context, text, duration).show();
+
+            }
+        });
+
+
+        RegisterLink.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
             }
@@ -33,8 +54,5 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void login(View view)
-    {
-       //login code
-    }
+
 }
